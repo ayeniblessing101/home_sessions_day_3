d9cd97ec3101b1240e5c49017bd63f524492fb31
Blessing <ayeniblessing32@gmail.com>
2017-06-08 19:21:44 +0100

    Add fonts

---
+++ coding_labs_2/src/stringSplosion.js
@@ -0,0 +1,18 @@
+class StringSplosion{
+  constructor(string)
+  {
+    this.string = string;
+  }
+
+  manipulate(){
+    let exploded = "";
+    let strExplode = this.string.split(" ");
+    for(let i=0; i<strExplode.length; i++){
+      for(let j=0; j<i; j++){
+        //return j;
+      }
+      //return exploded;
+    }
+  }
+}
+  
